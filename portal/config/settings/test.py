b688a46af410f095c315c632d541917fd6806fa9
itsyourdecide <vinnikivan0@gmail.com>
2026-09-10 14:48:24 +0000

    feat(portal): configure PostgreSQL database

--- portal/config/settings/test.py
+++ portal/config/settings/test.py
@@ -3,3 +3,10 @@ from .base import *  # noqa: F403
 SECRET_KEY = "test-only-not-for-production"
 DEBUG = False
 ALLOWED_HOSTS = ["testserver"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
