f4116b0f1f30437f6863543312c60ce0b8f11c01
itsyourdecide <vinnikivan0@gmail.com>
2026-09-14 15:47:49 +0300

    refactor(portal): add application factory

--- portal/app/main.py
+++ portal/app/main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 
 from app.system.router import router
 
-app = FastAPI(
-    title="portal",
-    version="0.1.0",
-)
 
-app.include_router(router)
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="portal",
+        version="0.1.0",
+    )
+
+    app.include_router(router)
+    return app
+
+
+app = create_app()
